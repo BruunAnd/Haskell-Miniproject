2ae96f3f5490204e5e3b7fc097ac8c7fc6266edd
Anders Jakobsen <andersjkbsn@gmail.com>
2018-11-18 22:33:35 +0100

    Can now encode any ordered datatype

--- project.hs
+++ project.hs
@@ -3,7 +3,6 @@ import Data.List (sort)
 import Data.Set (toList, fromList)
 
 -- Helper function to insert an element into a sorted list
--- I believe 
 sortedInsert :: Ord a => a -> [a] -> [a]
 sortedInsert elem [] = [elem]
 sortedInsert elem (x:xs) = if elem <= x
@@ -11,19 +10,19 @@ sortedInsert elem (x:xs) = if elem <= x
                            else x : sortedInsert elem xs
 
 -- Given a char and a string, compute how often the char appears in the string
-charFreq :: Char -> String -> Int
-charFreq char str = length (filter (==char) str)
+elemFreq :: Ord a => a -> [a] -> Int
+elemFreq elem str = length (filter (==elem) str)
 
 -- To count character frequency, I count how frequently each unique character appears
 -- A simple approach is to convert the string to a set, s.t. there are no duplicates, and then converting it back
-uniqueList :: Ord a => [a] -> [a]
+uniqueList :: Ord a =>[a] -> [a]
 uniqueList = toList . fromList
 
 -- Create an association list from characters to their frequencies
-buildFreqList :: String -> FrequencyList Char
-buildFreqList str = let uniques = uniqueList str
+buildFreqList :: Ord a =>[a] -> FrequencyList a
+buildFreqList lst = let uniques = uniqueList lst
                     in
-                      map (\char -> (char, charFreq char str)) uniques
+                      map (\elem -> (elem, elemFreq elem lst)) uniques
 
 -- Type synonym for encoding map and frequency lists
 type FrequencyList a = [(a, Int)]
@@ -34,38 +33,39 @@ data Bit = Zero | One deriving (Show, Eq)
 type Bits = [Bit]
 
 -- Data type definition for the (Huffman binary tree)
-data BTree = Leaf Char Int | Branch BTree BTree Int deriving Show
+-- I have not put a constraint on the datatype because I would have to put the constraint on functions anyway
+data BTree a = Leaf a Int | Branch (BTree a) (BTree a) Int deriving Show
 
 -- Define equality on the BTree type. Two nodes are considered equal if they have the same frequency
-instance Eq BTree where
+instance (Ord a) => Eq (BTree a) where
     x == y = treeFreq x == treeFreq y
 
 -- Define ordering on the BTree type. The ordering is based on the frequency of the node
-instance Ord BTree where
+instance (Ord a) => Ord (BTree a) where
     compare x y = compare (treeFreq x) (treeFreq y)
 
 -- Given a BTree, treeFreq returns the frequency associated with the root node of the tree
-treeFreq :: BTree -> Int
-treeFreq (Leaf c f) = f
+treeFreq :: Ord a => BTree a -> Int
+treeFreq (Leaf e f) = f
 treeFreq (Branch l r f) = f
 
 -- BTrees can be merged such that, given two trees, the resulting tree branches to its two children and uses the sum of its frequencies as its frequency
-mergeTree :: BTree -> BTree -> BTree
+mergeTree :: Ord a => BTree a -> BTree a -> BTree a 
 mergeTree l r = Branch l r ((treeFreq l) + (treeFreq r))
 
 -- Maps an association list of char frequencies to corresponding leaves
-freqToLeaves :: FrequencyList Char -> [BTree]
+freqToLeaves :: Ord a => FrequencyList a -> [BTree a]
 freqToLeaves freqs = map (\(a, b) -> Leaf a b) freqs
 
 -- Construct a Huffman binary tree given a list of char-frequency pairs
-buildTree :: FrequencyList Char -> BTree
+buildTree :: Ord a => FrequencyList a -> BTree a
 buildTree list = buildTree' (sort (freqToLeaves list))
                  where
                     buildTree' [x] = x
                     buildTree' (x:y:r) = buildTree' (sortedInsert (mergeTree x y) r)
 
 -- Recursively construct encoding map given a BTree
-buildMap :: BTree -> EncodingMap Char
+buildMap :: Ord a => BTree a -> EncodingMap a
 buildMap tree = buildMap' tree []
                 where
                     -- From a branch, we will explore both the left and right branch
@@ -73,38 +73,36 @@ buildMap tree = buildMap' tree []
                     -- When we encounter a leaf, we return the reverse of the path needed to get to the associated char
                     buildMap' (Leaf char _) list = [(char, reverse list)]
 
--- Encode a character using a mapping
--- Using Prelude's lookup here, which returns a Maybe type
--- Nothing should never be returned unless there is a bug in the encoder
-encodeChar :: Char -> EncodingMap Char -> Bits
-encodeChar char mapping = let result = lookup char mapping
-                          in
-                            case result of
-                              Just value -> value
-                              Nothing -> error "Error during lookup"
+-- Encode a character using an encodingmap
+encodeElement :: Ord a => a -> EncodingMap a -> Bits
+encodeElement element mapping = let result = lookup element mapping
+                                in
+                                  case result of
+                                  Just value -> value
+                                  Nothing -> error "Error during lookup"
 
 -- Encode a string given a mapping
-encode' :: String -> EncodingMap Char -> Bits
-encode' str mapping = concatMap (\char -> encodeChar char mapping) str
+encode' :: Ord a => [a] -> EncodingMap a -> Bits
+encode' lst mapping = concatMap (\element -> encodeElement element mapping) lst
 
--- Encode a string and return its tree + the encoded string
-encode :: String -> Maybe (BTree, Bits)
-encode str = case str of 
-                "" -> Nothing
-                _  -> let tree = buildTree (buildFreqList str)
+-- Encode a list of equatable types and return its tree + t
+encode :: Ord a => [a] -> Maybe (BTree a, Bits)
+encode lst = case lst of 
+                [] -> Nothing
+                _  -> let tree = buildTree (buildFreqList lst)
                       in
-                        Just (tree, encode' str (buildMap tree))
+                        Just (tree, encode' lst (buildMap tree))
 
 -- Decode a string given a Huffman tree and bits
 -- The approach here is to traverse the tree until we encounter a leaf
 -- When we encounter a leaf, we return the char of that leaf and then start from the root again
-decode :: BTree -> Bits -> String
+decode :: Ord a => BTree a -> Bits -> [a]
 decode tree bits = decode' tree bits
                    where
                        -- In the case there are no more bits to consume, consume the last leaf
-                       decode' (Leaf char _) [] = [char]
+                       decode' (Leaf elem _) [] = [elem]
                        -- Unlike when choosing a bit, we do not consume a bit when we consume a leaf
-                       decode' (Leaf char _) all = char : decode' tree all
+                       decode' (Leaf elem _) all = elem : decode' tree all
                        -- When we encounter a branch, we consume a bit and follow the path
                        decode' (Branch l r _) (bit:rest) = if bit == Zero then decode' l rest else decode' r rest
 
